a54fb0cc8304c5288f71df58d350ad00373dd059
Francisco Costa <franciscomaiacosta12@gmail.com>
2025-09-16 13:09:13 +0100

    Replace Jekyll site with static Squarespace clone

--- assets/js/async-content-browser-dfd8203faa5f04dc-min.en-US.js
+++
@@ -1,3 +0,0 @@
-"use strict";(globalThis.webpackChunkextract_css=globalThis.webpackChunkextract_css||[]).push([["13792"],{625517:function(e,c,s){e.exports=s.p+"new-category-preview-e09825b21cde2fef.png"}}]);
-
-//# sourceMappingURL=https://sourcemaps.squarespace.net/universal/scripts-compressed/sourcemaps/fcb9b384bb9a82b9/async-content-browser-dfd8203faa5f04dc-min.en-US.js.map
